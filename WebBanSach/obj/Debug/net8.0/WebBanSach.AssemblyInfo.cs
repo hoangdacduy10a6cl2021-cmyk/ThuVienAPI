89c369775229a8e50de81a3de0f1467bc764e6c5
Tên của bạn <email@example.com>
2026-09-23 15:43:52 +0700

    dsadsad

--- WebBanSach/obj/Debug/net8.0/WebBanSach.AssemblyInfo.cs
+++ WebBanSach/obj/Debug/net8.0/WebBanSach.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBanSach")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b2220d6fd1175309a813c4460ae959cbceb7d73")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBanSach")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBanSach")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
